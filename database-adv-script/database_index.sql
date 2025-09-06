dd46a0237c001398521b9bc4e6eed91e1b28ec4d
xrisowen <xrisowen14@gmail.com>
2025-09-06 14:46:58 +0100

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,9 @@
-hhh
+CREATE INDEX idx_properties_location ON properties(location);
+CREATE INDEX idx_properties_name ON properties(name);
+
+EXPLAIN ANALYZE
+SELECT u.first_name, COUNT(b.booking_id) AS total_bookings
+FROM users u
+JOIN bookings b ON u.user_id = b.user_id
+GROUP BY u.first_name
+ORDER BY total_bookings DESC;
